40d9b4d28eaa6b87ed0cacb689c43b2ee6a5a7d0
Juan Lopes <juanfigarolopes@gmail.com>
2023-11-26 22:52:46 -0300

    Fixing pipeline

--- cards/mix.exs
+++ cards/mix.exs
@@ -14,8 +14,7 @@ defmodule Cards.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {YourAppName.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
